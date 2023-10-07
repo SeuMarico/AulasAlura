5ecb8eeae258794bc3983d4fbea1da0509d69aa7
Lucas <sergiorockemroll@gmail.com>
2023-10-07 17:00:00 -0300

    Flask Terminado

--- AulaFlask/jogoteca.py
+++ AulaFlask/jogoteca.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-from django.shortcuts import redirect
+from flask import Flask, render_template, request, redirect, session,flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -12,7 +11,22 @@ jogo2= Jogo('Skyrim', 'MMRPG', 'PS3')
 jogo3= Jogo('God of War', 'Rack n Slash', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
+class Usuario:
+    def __init__(self,nome,nickname,senha):
+        self.nome=nome
+        self.nickname=nickname
+        self.senha=senha
+
+usuario1 = Usuario('Lucas','Lutas','12345')
+usuario2 = Usuario('Carlos','Carlão','peneira')
+usuario3 = Usuario('José','Zé','caldo')
+
+usuarios ={ usuario1.nickname: usuario1, 
+            usuario2.nickname: usuario2, 
+            usuario3.nickname: usuario3}
+
 app = Flask(__name__)
+app.secret_key = 'candelabro'
 
 @app.route('/')
 def index():
@@ -21,6 +35,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 
@@ -32,5 +48,32 @@ def criar():
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' foi logado com sucesso')
+            proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
+    else:
+        flash('Usuario não encontrado')
+        return redirect('/login')
     
-app.run()
+@app.route('/deslogar')
+def deslogar():
+    session['usuario_logado'] = None
+    flash('Deslogado com sucesso')
+    return redirect('/')
+
+    
+app.run(debug=True)
